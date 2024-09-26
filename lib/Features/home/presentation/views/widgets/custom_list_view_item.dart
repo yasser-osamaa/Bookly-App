c9ff7c90d717b15416edf317e292f4cc8bccca73
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-26 19:14:16 +0300

    addingg box fit fill to book item

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +15,7 @@ class CustomListViewItem extends StatelessWidget {
             borderRadius: BorderRadius.circular(16),
             color: Colors.red,
             image: const DecorationImage(
+              fit: BoxFit.fill,
               image: AssetImage(
                 AssetsData.testImg,
               ),
