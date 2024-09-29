28260c1413529e85da912297eb53588e1a629bd4
yasser-osamaa <yasserosamaa88@gmail.com>
2024-09-29 18:22:08 +0300

    create book detailes view --> empty

---
+++ lib/Features/home/presentation/views/book_detailes_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/home/presentation/views/widgets/book_detailes_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailesView extends StatelessWidget {
+  const BookDetailesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailesViewBody(),
+    );
+  }
+}
