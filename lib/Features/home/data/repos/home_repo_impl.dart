6519af3cb03a0f9e26b4283363d2e2e35d4f5c26
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-02 20:53:52 +0300

    create api service this class can use it in any application

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+}
