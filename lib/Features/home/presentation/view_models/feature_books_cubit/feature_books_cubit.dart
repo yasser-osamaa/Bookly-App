1215938c0a777b81f13500123d66d94360656b48
yasser-osamaa <yasserosamaa88@gmail.com>
2024-10-03 15:26:30 +0300

    feature books method done

--- lib/Features/home/presentation/view_models/feature_books_cubit/feature_books_cubit.dart
+++ lib/Features/home/presentation/view_models/feature_books_cubit/feature_books_cubit.dart
@@ -1,9 +1,24 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubitCubit extends Cubit<FeatureBooksCubitState> {
-  FeatureBooksCubitCubit() : super(FeatureBooksCubitInitial());
+  FeatureBooksCubitCubit({required this.homeRepo})
+      : super(FeatureBooksCubitInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFetureBooks() async {
+    emit(FeatureBooksCubitLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+    result.fold(
+      (failure) {
+        emit(FeatureBooksCubitFailure(errMsg: failure.errMsg));
+      },
+      (books) {
+        emit(FeatureBooksCubitSuccess(books: books));
+      },
+    );
+  }
 }
